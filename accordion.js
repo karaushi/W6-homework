9a524e117edc627637c78b1753cdee17498e7783
T.Karaushi <karytatu@gmail.com>
2023-01-18 00:36:35 +0900

    first commit

---
+++ accordion.js
@@ -0,0 +1,56 @@
+(() => {
+    //イニシャルの文字は大文字
+	class Accordion {
+		
+		//初期化（先に実行させたい）
+        		constructor(obj){
+
+			console.log('obj', obj.hookName);
+
+			const $elm = document.querySelector(obj.hookName);
+			const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+			const triggerLen = $trigger.length;
+			let index = 0;
+			while (index < triggerLen) {
+				$trigger[index].addEventListener('click', (e) => this.clickHandler(e));	
+				index++;
+			}
+            //this.actionUnko();
+		}
+
+        /*actionUnko(){
+            console.log('Hello World Unko!');
+        }*/
+
+		//クリックしたら実行される処理
+		clickHandler(e){
+			e.preventDefault();
+
+			const $target = e.currentTarget;
+			const $content = $target.nextElementSibling;
+			
+			if($content.style.display === 'block'){
+				$content.style.display = 'none';
+			} else {
+				$content.style.display ='block';
+			}
+		}
+	}
+
+	const fuckingAccordion = new Accordion({
+		hookName: '#js-faq',
+		tagName: 'p'
+	});
+
+	const dummyAccordion = new Accordion({
+		hookName: '#js-accordion',
+		tagName: 'a'
+	});
+
+	const miniAccordion = new Accordion({
+		hookName: '#js-accordion-mini',
+		tagName: 'dt'
+	});
+
+})();
